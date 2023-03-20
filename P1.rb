f6fc6489454720268366a361c63d9f9552d9fd22
Christian <95581221+Christe06@users.noreply.github.com>
2023-03-20 10:58:06 -0600

    Add files via upload

---
+++ P1.rb
@@ -0,0 +1,13 @@
+capitales = {
+    "Mexico" => "Ciudad de Mexico",
+    "Colombia" => "Bogota",
+    "Peru" => "Lima",
+    "Chile" => "Santiago de Chile",
+    "España" => "Madrid",
+    "Argentina" => "Buenos aires",
+    "Paises bajos" => "Amsterdam",
+    "Estados Unidos" => "Washington"
+}
+
+puts capitales.size
+puts capitales.empaty?
---
+++ p2-condicionale.rb
@@ -0,0 +1,7 @@
+is_auntheticated = false 
+
+if is_auntheticated
+    puts "pantalla admin "
+else 
+    puts "pantalla 2" 
+end                                                                   
